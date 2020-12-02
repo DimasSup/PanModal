24dbb753f6210642d9f924cdce8b7138f5948068
Dimas <DimasSup@gmail.com>
2020-12-02 04:33:54 +0200

    no message

--- PanModal/View/PanContainerView.swift
+++ PanModal/View/PanContainerView.swift
@@ -25,7 +25,14 @@ class PanContainerView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    override var backgroundColor: UIColor?{
+        set{
+            super.backgroundColor = newValue
+        }
+        get{
+            return UIColor.clear
+        }
+    }
 }
 
 extension UIView {
